c481fc5c3a15d5c9811f546c5ce97b24311a897e
student15 <student15@pcoip.ki.agh.edu.pl>
2019-06-01 13:01:08 +0200

    inital maven project

---
+++ src/main/java/maven/unasdziala/App.java
@@ -0,0 +1,13 @@
+package maven.unasdziala;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
