2f2d98872d2a8c318ea51c5177c9f32614af41aa
student15 <student15@pcoip.ki.agh.edu.pl>
2019-06-01 15:44:55 +0200

    add print

--- src/main/java/maven/unasdziala/Console.java
+++ src/main/java/maven/unasdziala/Console.java
@@ -1,11 +1,23 @@
 package maven.unasdziala;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 import java.util.Scanner;
 
 public class Console {
-	public static void main(String[] args) {
+	public static void main(String[] args)throws ParseException {
+		Options options = new Options();
+		CommandLineParser parser = new DefaultParser();
+	     CommandLine cmd = parser.parse( options, args);
+	     System.out.println("Give me a year");
+			Scanner y = new Scanner(System.in);
+			int year=y.nextInt();
 
 		boolean end = false;
+		
 
 		do {
 			Scanner scan = new Scanner(System.in);
